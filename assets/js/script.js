84fb48e4a02abc4ca196323bc4ae3f8ac2c219da
CRMclaughlin <chrisraymusik@gmail.com>
2022-10-22 16:17:43 -0500

    appended elements to dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,22 +18,29 @@
 
 
 // display the data 
-// one day forcast, use css to create the UI around that. 
+// one day forcast, use css to create the UI around that.
+
 // 5 day forcast and update the UI
 // then Ill worry about searching and inputs 
 //  Save past searches cities to local storage 
 
 
 
-var apiKey = '5263d662f1f882f01bbcb8a4ee9c63ef';
-var userCityEl = $('city-input');
-var currentWeatherEl = $('#currentConditions');
+const apiKey = '5263d662f1f882f01bbcb8a4ee9c63ef';
+let userCityEl = $('city-input');
+let currentWeatherEl = $('#currentConditions');
 var fiveDayEl = $('#fiveDayForcast');
-var pastCitySearch = ('#past-searches');
-var today = moment().format('MM/DD/YYYY');
-var submitBtn = document.querySelector('form');
+let pastCitySearch = ('#past-searches');
+let submitBtn = document.querySelector('form');
 
-var userCity = ""
+let userCity = ""
+
+let currentCityEl = $('city');
+let currentTempEl = $('temperature');
+let currentWindSpeedEl = $('wind-speed');
+let currentHumidityEl = $('humid');
+var fiveDayEl = document.querySelector('.five-day');
+let icon = $('weather-icon');
 
 
 
@@ -42,62 +49,75 @@ var userCity = ""
 
 function getWeather(event) {
     event.preventDefault()
-    userCity = $('#city-input').val()
-    console.log(userCity)
+    userCity = $('#city-input').val();
+
 
     // api call to get weather for city
     let citySearch = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&units=imperial&appid=${apiKey}`;
     fetch(citySearch)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data){
-        console.log(data)
-       // let temp = Math.floor(data.main);
-        // console.log(data.wind.speed, Math.floor(data.main.temp));
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+          
 
-        var currentConditionsEl= $('#currentConditions');
-        currentConditionsEl.addClass('border border-primary');
+            var currentConditionsEl = $('#currentConditions');
+            currentConditionsEl.addClass('border border-primary');
 
-        // var cityNameEl = $('<h2>')
-        // cityNameEl.text('city-input');
-        // currentConditionsEl.append(cityNameEl)
-        // creates elements in the dom
-        var h1 = document.createElement('h1');
-        var h2 = document.createElement('h2');
-        var img = document.createElement('img');
-        var h3Temp = document.createElement('h3');
-        var h3Wind = document.createElement('h3')
-        var h3Hum = document.createElement('h3')
-        var h4 = document.createElement('h4')
+            var today = moment().format('MM/DD/YYYY');
+            const icon = data.weather[0].icon;
+            const temp = data.main.temp;
+            const wind = data.wind.speed;
+            const humidity = data.main.humidity;
+            
+            var h1 = document.createElement('h1');
+            var img = document.createElement('img');
+            var h3Temp = document.createElement('h3');
+            var h3Wind = document.createElement('h3');
+            var h3Hum = document.createElement('h3');
+            
+            
+            // creates elements in the dom
+            h1.textContent = userCity + '(' + today + ')';
+            h3Temp.textContent = 'Temp: ' + temp + '°F';
+            h3Wind.textContent = 'Wind: ' + wind + ' MPH ';
+            h3Hum.textContent = "Humidity: " + humidity + "%";
+            img.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+            
+            // appends elements to the dom
+             currentWeatherEl.append(h1, img, h3Temp, h3Wind, h3Hum);
+          
 
-        h1.textContent = today
-        h2.textContent = userCity
-        h3Temp.textContent = data.main.temp;
-        h3Wind.textContent = data.wind.speed;
-        h3Hum.textContent = data.main.humidity;
-        // img.src = data.weather[0].icon;
+            // fiveDayForcast(userCity)
 
-        // var icon = data.weather[0].icon;
-        // var temp = data.main.temp;
-        // var wind = data.wind.speed;
-        // var humidity = data.main.humidity;
-        // var date = new Date().toLocaleDateString;
-
-
-        // appends elements to the dom
-        currentWeatherEl.append(today,userCity, h3Temp, h3Wind, h3Hum, img);
-        
-        // currentWeatherEl.append(h3)
-        // currentWeatherEl.append(img)
-       
-        
-    })
+        })
 }
+
+// function fiveDayForcast(userCity) {
+//     let fiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${userCity}&units=imperial&appid=${apiKey}`
+//     fetch(fiveDay)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data)
+//         })
+
+//         document.getElementById('fiveDayForecastCards').style.display = 'flex';
+
+//         let boxes = document.querySelectorAll('#weather');
+        
+//         for (i = 0; i < boxes.length; i++){
+
+//         }
+            
+
+// }
+
 
 submitBtn.addEventListener('submit', getWeather)
 
-
+// let weatherMain = i * 8 + 3;
 
 // function handleSubmit(event) {
 //     event.preventDefault();
